3d9cb9ab50594d3a69a3cdb1c3bf37bc8c9a8df7
SHIVUshivuonmicrosoftcom <webcartshivu@gmail.com>
2024-11-03 22:14:26 +0530

    Comitting the TFL Implimentation

---
+++ Source/TFL.PlugIns.ObservationPhotos/AddTFLLogo.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using static System.Net.WebRequestMethods;
+using System.Reflection;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IdentityModel.Metadata;
+using System.Runtime.Remoting.Services;
+
+namespace TFL.PlugIns.ObservationPhotos
+{
+    public class AddTFLLogo : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory =(IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var traceing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+            traceing.Trace("Started Plug in");
+
+                traceing.Trace($"Depth is  {context.Depth} ");
+            //if (context.Depth > 2) return;
+                var target = context.InputParameters["Target"] as Entity;
+                if (target !=null)
+                {
+                    var initializeFileBlocksDownloadRequest = new InitializeFileBlocksDownloadRequest
+                    {
+                        Target = new EntityReference(target.LogicalName, target.Id),
+                        FileAttributeName = "sp_photo"
+                    };
+
+                    var initializeFileBlocksDownloadResponse = (InitializeFileBlocksDownloadResponse)
+                        service.Execute(initializeFileBlocksDownloadRequest);
+                    traceing.Trace($"initilising Download Block request");
+                    DownloadBlockRequest downloadBlockRequest = new DownloadBlockRequest
+                    {
+                        FileContinuationToken = initializeFileBlocksDownloadResponse.FileContinuationToken
+                    };
+                    var downloadBlockResponse = (DownloadBlockResponse)service.Execute(downloadBlockRequest);
+
+                    traceing.Trace($"Calling Method to Place  Logo");
+                    byte[] newIimageByte = OverLayImage(downloadBlockResponse.Data);
+                    traceing.Trace($"Calling Method to Update the new file back to Record");
+                    UploadFile(traceing,service, "sp_observationphoto", target.Id, "sp_photo", newIimageByte,"AddedLogo");
+                }            
+        }
+        private static void UploadFile(ITracingService traceing, IOrganizationService svc, string entityName, Guid recordGuid,
+       string fileAttributeName, byte[] filebyte, string fileName)
+        {
+            // get the file content in byte array
+            var fileContentByteArray = filebyte;
+            traceing.Trace($"Initilising Block Upload Request");
+            var initializeFileBlocksUploadRequest = new InitializeFileBlocksUploadRequest()
+            {
+                Target = new EntityReference(entityName, recordGuid),
+                FileAttributeName = fileAttributeName,
+                FileName = fileName
+            };
+            traceing.Trace($"Executing  Block Upload Request");
+            var initializeFileBlocksUploadResponse = (InitializeFileBlocksUploadResponse)
+                svc.Execute(initializeFileBlocksUploadRequest);
+            var lstBlock = new List<string>();
+            if (fileContentByteArray.Length< 4194304)
+            {
+                var blockId = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+                lstBlock.Add(blockId);
+                traceing.Trace($"Initilising UploadBlockRequest in side if bloack");
+                var uploadBlockRequest = new UploadBlockRequest()
+                {
+                    BlockId = blockId,
+                    BlockData = fileContentByteArray.ToArray(),
+                    FileContinuationToken = initializeFileBlocksUploadResponse.FileContinuationToken
+                };
+
+                var uploadBlockResponse = (UploadBlockResponse)svc.Execute(uploadBlockRequest);
+                traceing.Trace($"Received the  UploadBlockResponse");
+                traceing.Trace($"Initilising   CommitBlock");
+
+                var commitFileBlocksUploadRequest = new CommitFileBlocksUploadRequest
+                {
+                    FileContinuationToken = initializeFileBlocksUploadResponse.FileContinuationToken,
+                    FileName = fileName,
+                    MimeType = "image/jpeg",
+                    BlockList = lstBlock.ToArray()
+
+                };
+                
+                var commitFileBlocksUploadResponse = (CommitFileBlocksUploadResponse)svc.Execute(commitFileBlocksUploadRequest);
+                traceing.Trace($"CommitBlock Completed");
+            }
+            else
+            {
+                traceing.Trace($"Initilising UploadBlockRequest Else if bloack");
+                for (int i = 0; i < fileContentByteArray.Length / 4194304; i++)
+                {
+                    var blockId = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+                    lstBlock.Add(blockId);
+                    var uploadBlockRequest = new UploadBlockRequest()
+                    {
+                        BlockId = blockId,
+                        BlockData = fileContentByteArray.Skip(i * 4194304).Take(4194304).ToArray(),
+                        FileContinuationToken = initializeFileBlocksUploadResponse.FileContinuationToken
+                    };
+
+                    var uploadBlockResponse = (UploadBlockResponse)svc.Execute(uploadBlockRequest);
+                    traceing.Trace($"Received the  UploadBlockResponse Else Block");
+                    traceing.Trace($"Initilising   CommitBlock Else Block" );
+
+                    var commitFileBlocksUploadRequest = new CommitFileBlocksUploadRequest
+                    {
+                        FileContinuationToken = initializeFileBlocksUploadResponse.FileContinuationToken,
+                        FileName = fileName,
+                        MimeType = "image/jpeg",
+                        BlockList = lstBlock.ToArray()
+
+                    };
+
+                    var commitFileBlocksUploadResponse = (CommitFileBlocksUploadResponse)svc.Execute(commitFileBlocksUploadRequest);
+                    traceing.Trace($"CommitBlock Completed Else Block");
+                }
+            }
+        }
+
+        private byte[] OverLayImage(byte[] databytes)
+        {
+            try
+            {
+                using (var ms= new MemoryStream(databytes))
+                using (var bitmap= new Bitmap(ms))
+                using (var g= Graphics.FromImage(bitmap))
+                using (var pen = new Pen(ColorTranslator.FromHtml("#0019a7"), 6))
+                using (var brush = new SolidBrush(ColorTranslator.FromHtml("#0019a8")))
+                using (var output = new MemoryStream())
+                {
+                    g.SmoothingMode=SmoothingMode.AntiAlias;
+                    g.DrawEllipse(pen, bitmap.Width - 37, bitmap.Height - 41, 26, 26);
+                    g.DrawRectangle(pen, bitmap.Width - 44, bitmap.Height - 31, 40, 6);
+                    bitmap.Save(output, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    return output.ToArray();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Source/TFL.PlugIns.ObservationPhotos/JavaScript.js
@@ -0,0 +1,143 @@
+﻿function createCase(formContext) {
+	try
+	{
+		//debugger;
+		var parameters = {};
+		parameters["title"] = formContext.getAttribute("sp_observationtitle").getValue();
+		parameters["caseorigincode"] = 778390001;
+		var lookupValue = new Array();
+		lookupValue[0] = new Object();
+		lookupValue[0].id = formContext.data.entity.getId();
+		lookupValue[0].name = formContext.getAttribute("sp_observationtitle").getValue();
+		lookupValue[0].entityType = "sp_observation";
+		parameters["sp_observationid"] = lookupValue;
+		var entityFormOptions = {};
+		entityFormOptions["entityName"] = "incident";
+		var record = {};
+		record.statecode = 1; // State
+		record.statuscode = 778390001; // Status
+		Xrm.WebApi.updateRecord("sp_observation", formContext.data.entity.getId(), record).then(
+			function success(result) {
+			},
+			function (error) {
+			}
+		);
+		Xrm.Navigation.openForm(entityFormOptions, parameters).then(
+			function (success) {
+
+			},
+			function (error) {
+			});
+		
+	}
+
+	catch (ex) {
+	}
+}
+async function CreateCustomer(executionContext) {
+	try {
+		debugger;
+		var formContext = executionContext.getFormContext();
+		if (formContext.ui.getFormType() === 1) {
+			if (formContext.getAttribute("sp_observationid").getValue() !== null) {
+				var contactId = await getContactId(formContext, formContext.getAttribute("sp_observationid").getValue()[0].id.replace("{", "").replace("}", ""))
+
+			}
+		}
+
+
+	}
+
+	catch (ex) {
+	}
+
+}
+async function getContactId(formContext, observationId) {
+	try {
+		var contactId = null;
+		var firstName = null; var lastName = null;
+		var emailAddress = null;
+		var createdBy = null;
+		var userfullname = null;
+		debugger;
+		var result = await Xrm.WebApi.retrieveRecord("sp_observation", observationId, "?$select=_createdby_value").then(
+			function success(result) {
+				// Columns
+				var createdby = result["_createdby_value"]; // Lookup
+				debugger;
+				if (result["_createdby_value"] !== null) {
+					createdBy = result["_createdby_value"];
+
+				}
+
+			},
+			function (error) {
+			}
+		);
+
+		if (createdBy !== null) {
+			var userresult = await Xrm.WebApi.retrieveRecord("systemuser", createdBy, "?$select=_createdby_value,internalemailaddress,firstname,lastname,fullname").then(
+				function success(result) {
+					debugger;
+					var internalemailaddress = result["internalemailaddress"]; // Text
+					firstName = result["firstname"];
+					lastName = result["lastname"];
+					userfullname = result["fullname"];
+					emailAddress = result["internalemailaddress"];
+					if (result["internalemailaddress"] !== "") {
+						emailAddress = result["internalemailaddress"];
+
+					}
+				},
+				function (error) {
+				}
+			);
+		}
+		if (emailAddress !== null) {
+			var contactres = await Xrm.WebApi.retrieveMultipleRecords("contact", "?$select=contactid,_createdby_value,emailaddress1,fullname&$filter=emailaddress1 eq '" + emailAddress + "'").then(
+				function success(results) {
+					debugger;
+					for (var i = 0; i < results.entities.length; i++) {
+						var result = results.entities[i];
+						// Columns
+						//return result["contactid"]; // Guid
+						var lookupValue = new Array();
+						lookupValue[0] = new Object();
+						lookupValue[0].id = result["contactid"];
+						lookupValue[0].name = result["fullname"];
+						lookupValue[0].entityType = "contact";
+						formContext.getAttribute("customerid").setValue(lookupValue);
+					}
+				},
+				function (error) {
+					console.log(error.message);
+				}
+			);
+
+			var record = {};
+			record.firstname = firstName; // Text
+			record.lastname = lastName; // Text
+			record.emailaddress1 = emailAddress; // Text
+
+			var res = await Xrm.WebApi.createRecord("contact", record).then(
+				function success(result) {
+					contactId = result.id;
+					var lookupValue = new Array();
+					lookupValue[0] = new Object();
+					lookupValue[0].id = result.id;;
+					lookupValue[0].name = userfullname;
+					lookupValue[0].entityType = "contact";
+					formContext.getAttribute("customerid").setValue(lookupValue);
+				},
+				function (error) {
+				}
+			);
+
+		}
+
+	}
+
+	catch (ex) {
+	}
+
+}
